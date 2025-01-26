ae39ca07d67b36cfed6dff79ad168eb5b9ce955a
handoko <srabutdotcom@gmail.com>
2025-01-26 22:46:36 +0700

    add PskExchangeModes

--- src/pskmode.js
+++ src/pskmode.js
@@ -1,15 +1,30 @@
 // deno-lint-ignore-file no-slow-types
 // @ts-self-types="../type/pskmode.d.ts"
 
+import { Constrained, Uint8 } from "./dep.ts";
 import { Enum } from "./enum.js";
+import { parseItems } from "./utils.js";
 
 /**
  * PskKeyExchangeMode - @see https://datatracker.ietf.org/doc/html/rfc8446#section-4.4.2
  */
 export class PskKeyExchangeMode  extends Enum {
    
+   
+   /**
+    * PSK-only key establishment
+    * In this mode, the server
+      MUST NOT supply a "key_share" value.
+    */
    static PSK_KE = new PskKeyExchangeMode('PSK_KE', 0);
 
+   
+   /**
+    * PSK with (EC)DHE key establishment
+    * In this mode, the
+      client and server MUST supply "key_share" values as described in
+      Section 4.2.8.
+    */
    static PSK_DHE_KE = new PskKeyExchangeMode('PSK_DHE_KE', 1);
 
    static from(octet) {
@@ -23,5 +38,22 @@ export class PskKeyExchangeMode  extends Enum {
    get Uint8() { return Uint8Array.of(+this)}
 
 }
+
+export class PskKeyExchangeModes extends Constrained {
+   static from(array){
+      const copy = Uint8Array.from(array);
+      const lengthOf = Uint8.from(copy).value;
+      const ke_modes = parseItems(copy, 1, lengthOf, PskKeyExchangeMode);
+      return new PskKeyExchangeModes(...ke_modes)
+   }
+   constructor(...ke_modes) {
+      super(1, 255, ...ke_modes.map(e => e.Uint8))
+      this.ke_modes = ke_modes;
+   }
+}
+
+/* const test = new PskKeyExchangeModes(PskKeyExchangeMode.PSK_KE);
+const back = PskKeyExchangeModes.from(test);  */
+
 
 // npx -p typescript tsc ./src/pskmode.js --declaration --allowJs --emitDeclarationOnly --lib ESNext --outDir ./dist
--- type/pskmode.d.ts
+++ type/pskmode.d.ts
@@ -1,3 +1,4 @@
+import { Constrained } from "../src/dep.ts";
 import { Enum } from "../src/enum.js";
 
 /**
@@ -5,23 +6,45 @@ import { Enum } from "../src/enum.js";
  * @see https://datatracker.ietf.org/doc/html/rfc8446#section-4.2.7.
  */
 export declare class PskKeyExchangeMode extends Enum {
-   /** PSK-only key establishment. The server MUST NOT supply a "key_share" value. */
-   static PSK_KE: PskKeyExchangeMode;
+  /** PSK-only key establishment. The server MUST NOT supply a "key_share" value. */
+  static PSK_KE: PskKeyExchangeMode;
 
-   /** PSK with (EC)DHE key establishment. Both the client and server MUST supply "key_share" values. */
-   static PSK_DHE_KE: PskKeyExchangeMode;
+  /** PSK with (EC)DHE key establishment. Both the client and server MUST supply "key_share" values. */
+  static PSK_DHE_KE: PskKeyExchangeMode;
 
-   /**
-    * Creates a PskKeyExchangeMode instance from an octet.
-    * @param octet - The octet value (Uint8Array) to parse.
-    * @returns {PskKeyExchangeMode} - The corresponding PskKeyExchangeMode instance.
-    * @throws {Error} - If the octet is not a valid PskKeyExchangeMode type.
-    */
-   static from(octet: Uint8Array): PskKeyExchangeMode;
+  /**
+   * Creates a PskKeyExchangeMode instance from an octet.
+   * @param octet - The octet value (Uint8Array) to parse.
+   * @returns {PskKeyExchangeMode} - The corresponding PskKeyExchangeMode instance.
+   * @throws {Error} - If the octet is not a valid PskKeyExchangeMode type.
+   */
+  static from(octet: Uint8Array): PskKeyExchangeMode;
 
-   /** Returns the bit value for the mode (8). */
-   readonly bit: number;
+  /** Returns the bit value for the mode (8). */
+  readonly bit: number;
 
-   /** Returns the Uint8Array representation of the mode. */
-   readonly Uint8: Uint8Array;
+  /** Returns the Uint8Array representation of the mode. */
+  readonly Uint8: Uint8Array;
+}
+
+export declare class PskKeyExchangeModes extends Constrained {
+  /**
+   * Parses a `PskKeyExchangeModes` instance from a `Uint8Array`.
+   * @param array - The input array containing PSK key exchange mode data.
+   * @returns A new instance of `PskKeyExchangeModes`.
+   * @throws {Error} If the input array is invalid or incomplete.
+   */
+  static from(array: Uint8Array): PskKeyExchangeModes;
+
+  /**
+   * Constructs a `PskKeyExchangeModes` instance.
+   * @param ke_modes - A list of PSK key exchange modes.
+   * @throws {Error} If the constraints are not satisfied.
+   */
+  constructor(...ke_modes: PskKeyExchangeMode[]);
+
+  /**
+   * The list of PSK key exchange modes.
+   */
+  readonly ke_modes: PskKeyExchangeMode[];
 }
