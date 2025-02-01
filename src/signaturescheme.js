0d96de4835b09fcb2fa20291c324f7b246bd2110
handoko <srabutdotcom@gmail.com>
2025-02-01 12:21:17 +0700

    remove CertificateVerify and Finished

--- src/signaturescheme.js
+++ src/signaturescheme.js
@@ -1,10 +1,8 @@
 // deno-lint-ignore-file no-slow-types
 // @ts-self-types="../type/signaturescheme.d.ts"
 
-import { Constrained, Struct, Uint16 } from "./dep.ts";
+import { Uint16 } from "./dep.ts";
 import { Enum } from "./enum.js";
-import { sha256, sha384, sha512 } from "@noble/hashes/sha2"
-import { HandshakeType } from "./handshaketype.js";
 
 /**
  * Enumeration of signature schemes as defined in RFC 8446.
@@ -113,150 +111,7 @@ export class SignatureScheme extends Enum {
       }
    }
 
-   async certificateVerify(clientHelloMsg, serverHelloMsg, encryptedExtensionsMsg, certificateMsg, RSAprivateKey) {
-      const signature = await signatureFrom(clientHelloMsg, serverHelloMsg, encryptedExtensionsMsg, certificateMsg, RSAprivateKey, this.algo)
-      return new CertificateVerify(this, signature)
-   }
 }
 
-export class CertificateVerify extends Uint8Array {
-   static fromMsg(array) {
-      const copy = Uint8Array.from(array)
-      const algorithm = SignatureScheme.from(copy.subarray(4));
-      const signature = Signature.from(copy.subarray(6))
-      return new CertificateVerify(algorithm, signature.opaque)
-   }
-   static from = CertificateVerify.fromMsg
-   constructor(signatureScheme, signature) {
-      const signatureConstrained = new Signature(signature);
-      const struct = new Struct(
-         signatureScheme.Uint16,
-         signatureConstrained
-      )
-      super(struct);
-      this.algorithm = signatureScheme;
-      this.signature = signature
-      return HandshakeType.CERTIFICATE_VERIFY.handshake(this);
-   }
-}
-
-export class Signature extends Constrained {
-   static from(array) {
-      const copy = Uint8Array.from(array);
-      const lengthOf = Uint16.from(copy).value;
-      return new Signature(copy.subarray(2, 2 + lengthOf))
-   }
-   constructor(opaque) {
-      super(0, 2 ** 16 - 1, opaque)
-      this.opaque = opaque
-   }
-}
-
-async function signatureFrom(clientHelloMsg, serverHelloMsg, encryptedExtensionsMsg, certificateMsg, RSAprivateKey, algo) {
-   const leading = Uint8Array.of(
-      //NOTE 64 space characters 
-      32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
-      //NOTE 'TLS 1.3, server CertificateVerify'
-      84, 76, 83, 32, 49, 46, 51, 44, 32, 115, 101, 114, 118, 101, 114, 32, 67, 101, 114, 116, 105, 102, 105, 99, 97, 116, 101, 86, 101, 114, 105, 102, 121,
-      //NOTE single null char
-      0
-   )
-
-   const hash = hashFromAlgo(algo)
-
-   const transcriptHash = hash
-      .update(clientHelloMsg)
-      .update(serverHelloMsg)
-      .update(encryptedExtensionsMsg)
-      .update(certificateMsg)
-      .digest();
-
-   const data = Struct.createFrom(
-      leading,
-      transcriptHash
-   )
-
-   const signBuffer = await crypto.subtle.sign(
-      algo,
-      RSAprivateKey,
-      data
-   )
-
-   /* const verify = await crypto.subtle.verify(
-         {
-            name: "RSA-PSS",//'RSASSA-PKCS1-v1_5',
-            saltLength: 256 / 8
-         },
-         RSAPublicKey, //rsapublickey in Certificate
-         sign,
-         data
-   ) */
-   return new Uint8Array(signBuffer)
-}
-
-function hashFromAlgo(algo) {
-   let sha
-   const { hash, saltLength } = algo;
-   if (hash) { sha = parseInt(hash.split("-")[1]); }
-   else if (saltLength) { sha = saltLength * 8 }
-   else { sha = 256 };
-   switch (sha) {
-      case 384: return sha384.create();
-      case 512: return sha512.create();
-      case 256:
-      default:
-         return sha256.create();
-   }
-}
-
-export async function finished(finishedKey, sha = 256, ...messages) {
-   //const finishedKey = hkdfExpandLabel(serverHS_secret, 'finished', new Uint8Array, 32);
-   const finishedKeyCrypto = await crypto.subtle.importKey(
-      "raw",
-      finishedKey,
-      {
-         name: "HMAC",
-         hash: { name: `SHA-${sha}` },
-      },
-      true,
-      ["sign", "verify"]
-   );
-
-   const hash = sha == 256 ? sha256.create() :
-      sha == 384 ? sha384.create() : sha256.create();
-
-   const messagesStruct = Struct.createFrom(...messages);
-
-   const transcriptHash = hash
-      .update(Uint8Array.from(messagesStruct))
-      .digest();
-
-   const verify_data = await crypto.subtle.sign(
-      { name: "HMAC" },
-      finishedKeyCrypto,
-      transcriptHash
-   )
-
-   /* const _test_verify_data = await crypto.subtle.verify(
-      { name: "HMAC" },
-      finishedKeyCrypto,
-      verify_data,
-      transcriptHash
-   ) */
-   //verify_data.transcriptHash = transcriptHash;
-   return new Finished(verify_data);
-}
-
-export class Finished extends Uint8Array {
-   static fromMsg(message) {
-      const copy = Uint8Array.from(message)
-      return new Finished(copy.subarray(4))
-   }
-   constructor(verify_data) {
-      super(verify_data);
-      this.verify_data = verify_data
-      return HandshakeType.FINISHED.handshake(this)
-   }
-}
 
 // npx -p typescript tsc ./src/signaturescheme.js --declaration --allowJs --emitDeclarationOnly --lib ESNext --outDir ./dist
