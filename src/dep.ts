d8cd64f42d706ab5f416f1e1668d1684bf2a1b43
handoko <srabutdotcom@gmail.com>
2025-04-03 01:56:31 +0700

    update dependency @tls/struct version

--- src/dep.ts
+++ src/dep.ts
@@ -5,7 +5,6 @@ export { p521 } from '@noble/curves/p521'
 export { x25519 } from '@noble/curves/ed25519'
 export { x448 } from '@noble/curves/ed448'
 export * as utils from "@noble/curves/abstract/utils"
-export { HexaDecimal } from "@tls/struct"
 export * as x509 from "@peculiar/x509"
 export * from "@noble/hashes/sha2"
 
